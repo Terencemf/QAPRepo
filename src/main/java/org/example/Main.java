f9ced21114f18b2c1f94459a3e70f45b853b281b
Terence <terencemf@hotmail.com>
2022-12-02 15:59:25 -0330

    Revert

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! and Suresh");
         System.out.println("This is a test.");
-        System.out.println("Changes made ");
     }
 }
