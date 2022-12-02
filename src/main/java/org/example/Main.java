5b16410034a9f31573f9f73cd71734059cb8736c
Terence <terencemf@hotmail.com>
2022-12-02 15:49:02 -0330

    Commit and push clone

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! and Suresh");
         System.out.println("This is a test.");
+        System.out.println("Changes made ");
     }
 }
